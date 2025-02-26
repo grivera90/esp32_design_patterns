f68dea524043dab6981936a7218e41fb4c3bf9bc
grivera <grivera@systel.com.ar>
2025-02-26 15:09:13 -0300

    tca9554 driver develop

--- design_pattern_factory/components/tca9554_drv/include/tca9554_drv.h
+++ design_pattern_factory/components/tca9554_drv/include/tca9554_drv.h
@@ -28,6 +28,13 @@
 /******************************************************************************
         Data types
  ******************************************************************************/
+typedef enum
+{
+	TCA9554_OK = 0,
+	TCA9554_ERROR
+	
+} tca9554_ret_t;
+
 /**
  * @brief this struct represent the bits in Input Register. 
  */
@@ -52,8 +59,78 @@ union tca9554_input_reg
 	struct tca9554_single_input_reg bit;
 };
 
+struct tca9554_single_ouput_reg
+{
+	uint8_t p0:1;
+	uint8_t p1:1;
+	uint8_t p2:1;
+	uint8_t p3:1;
+	uint8_t p4:1;
+	uint8_t p5:1;
+	uint8_t p6:1;
+	uint8_t p7:1;
+};
+
+union tca9554_output_reg
+{
+	uint8_t all;
+	struct tca9554_single_ouput_reg bit;
+};
+
+struct tca9554_single_polarity_reg
+{
+	uint8_t p0:1;
+	uint8_t p1:1;
+	uint8_t p2:1;
+	uint8_t p3:1;
+	uint8_t p4:1;
+	uint8_t p5:1;
+	uint8_t p6:1;
+	uint8_t p7:1;
+};
+
+union tca9554_polarity_reg
+{
+	uint8_t all;
+	struct tca9554_single_polarity_reg bit;
+};
+
+struct tca9554_single_config_reg
+{
+	uint8_t p0:1;
+	uint8_t p1:1;
+	uint8_t p2:1;
+	uint8_t p3:1;
+	uint8_t p4:1;
+	uint8_t p5:1;
+	uint8_t p6:1;
+	uint8_t p7:1;
+};
+
+union tca9554_config_reg
+{
+	uint8_t all;
+	struct tca9554_single_config_reg bit;
+};
+
+
+/**
+ * @brief struct of unions the registers. 
+ */
+typedef struct 
+{
+	union tca9554_input_reg inputs;
+	union tca9554_output_reg outputs;
+	union tca9554_polarity_reg polarity;
+	union tca9554_config_reg config;
+		
+} tca9554_regs_t;
+
 typedef struct
 {
+	tca9554_regs_t reg;
+	int (*i2c_write_byte)(uint8_t slave_addr, uint8_t reg_addr, uint8_t reg_value);
+	int (*i2c_read_byte)(uint8_t slave_addr, uint8_t reg_addr, uint8_t *reg_value);
 	
 } tca9554_t;
 /******************************************************************************
@@ -63,7 +140,7 @@ typedef struct
 extern "C" {
 #endif
 
-
+tca9554_ret_t tca9554_init(tca9554_regs_t *reg);
 
 #ifdef __cplusplus
 } // extern "C"
