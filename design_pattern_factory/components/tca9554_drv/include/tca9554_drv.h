22a3653d6429658f18cad3315af33fe9ebd4030c
grivera90 <gonzaloriveras90@gmail.com>
2025-02-26 19:16:10 -0300

    tca9554 drv develop

--- design_pattern_factory/components/tca9554_drv/include/tca9554_drv.h
+++ design_pattern_factory/components/tca9554_drv/include/tca9554_drv.h
@@ -1,6 +1,6 @@
 /**
 *******************************************************************************
-* @file           : tca9554_drv.c
+* @file           : tca9554_drv.h
 * @brief          : Description of header file
 * @author         : Gonzalo Rivera
 * @date           : 25/02/2025
@@ -20,11 +20,6 @@
         Constants
  ******************************************************************************/
 #define TCA9554_ADDRESS				(0x27)
-
-#define TCA9554_INPUT_REG 			(0x00)		// Input status register
-#define TCA9554_OUTPUT_REG			(0x01)		// Output register to change state of output BIT set to 1, output set HIGH
-#define TCA9554_POLARITY_REG 		(0x02)		// Polarity inversion register. BIT '1' inverts input polarity of register 0x00
-#define TCA9554_CONFIG_REG			(0x03)		// Configuration register. BIT = '1' sets port to input BIT = '0' sets port to output
 /******************************************************************************
         Data types
  ******************************************************************************/
@@ -35,100 +30,67 @@ typedef enum
 	
 } tca9554_ret_t;
 
+typedef enum
+{
+	GPIO0 = 0,
+	GPIO1,
+	GPIO2,
+	GPIO3,
+	GPIO4,
+	GPIO5,
+	GPIO6,
+	GPIO7,
+	GPIO_MAX
+} tca9554_gpio_t;
+
+typedef enum
+{
+	TCA9554_INPUT_REG = 0,			// Input status register.
+	TCA9554_OUTPUT_REG,				// Output register to change state of output BIT set to 1, output set HIGH.
+	TCA9554_POLARITY_REG,			// Polarity inversion register. BIT '1' inverts input polarity of register 0x00.
+	TCA9554_CONFIG_REG				// Configuration register. BIT = '1' sets port to input BIT = '0' sets port to output.
+	
+} register_address_t;
 /**
- * @brief this struct represent the bits in Input Register. 
- */
-struct tca9554_single_input_reg
-{
-	uint8_t p0:1;
-	uint8_t p1:1;
-	uint8_t p2:1;
-	uint8_t p3:1;
-	uint8_t p4:1;
-	uint8_t p5:1;
-	uint8_t p6:1;
-	uint8_t p7:1;
-};
-
-/**
- * @brief this struct represent the all register and bits of Input Register. 
- */
-union tca9554_input_reg
-{
-	uint8_t all;
-	struct tca9554_single_input_reg bit;
-};
-
-struct tca9554_single_ouput_reg
-{
-	uint8_t p0:1;
-	uint8_t p1:1;
-	uint8_t p2:1;
-	uint8_t p3:1;
-	uint8_t p4:1;
-	uint8_t p5:1;
-	uint8_t p6:1;
-	uint8_t p7:1;
-};
-
-union tca9554_output_reg
-{
-	uint8_t all;
-	struct tca9554_single_ouput_reg bit;
-};
-
-struct tca9554_single_polarity_reg
-{
-	uint8_t p0:1;
-	uint8_t p1:1;
-	uint8_t p2:1;
-	uint8_t p3:1;
-	uint8_t p4:1;
-	uint8_t p5:1;
-	uint8_t p6:1;
-	uint8_t p7:1;
-};
-
-union tca9554_polarity_reg
-{
-	uint8_t all;
-	struct tca9554_single_polarity_reg bit;
-};
-
-struct tca9554_single_config_reg
-{
-	uint8_t p0:1;
-	uint8_t p1:1;
-	uint8_t p2:1;
-	uint8_t p3:1;
-	uint8_t p4:1;
-	uint8_t p5:1;
-	uint8_t p6:1;
-	uint8_t p7:1;
-};
-
-union tca9554_config_reg
-{
-	uint8_t all;
-	struct tca9554_single_config_reg bit;
-};
-
-
-/**
- * @brief struct of unions the registers. 
+ * @brief this typedef represent the bits in a register. 
  */
 typedef struct 
 {
-	union tca9554_input_reg inputs;
-	union tca9554_output_reg outputs;
-	union tca9554_polarity_reg polarity;
-	union tca9554_config_reg config;
-		
-} tca9554_regs_t;
+    uint8_t b0:1;
+    uint8_t b1:1;
+    uint8_t b2:1;
+    uint8_t b3:1;
+    uint8_t b4:1;
+    uint8_t b5:1;
+    uint8_t b6:1;
+    uint8_t b7:1;
+} bits_t;
 
+/**
+ * @brief this typedef represent the register complete and the bits of a register. 
+ */
+typedef struct 
+{
+	union 
+	{
+	    uint8_t all;
+	    bits_t bit;
+	} set;
+	    
+} register_t;
+
+/**
+ * @brief tca9554 device.  
+ */
 typedef struct
 {
-	tca9554_regs_t reg;
+	uint8_t slave_address;
+	
+	register_t input_register;
+	register_t output_register;
+	register_t polarity_register;
+	register_t config_register;
+	
 	int (*i2c_write_byte)(uint8_t slave_addr, uint8_t reg_addr, uint8_t reg_value);
 	int (*i2c_read_byte)(uint8_t slave_addr, uint8_t reg_addr, uint8_t *reg_value);
 	
@@ -140,7 +102,11 @@ typedef struct
 extern "C" {
 #endif
 
-tca9554_ret_t tca9554_init(tca9554_regs_t *reg);
+tca9554_ret_t tca9554_init(tca9554_t *tca9554, uint8_t address);
+tca9554_ret_t tca9554_write_reg(tca9554_t *tca9554, register_address_t reg_address, uint8_t reg_value);
+tca9554_ret_t tca9554_read_reg(tca9554_t *tca9554, register_address_t reg_address, uint8_t *reg_value);
+tca9554_ret_t tca9554_write_bit(tca9554_t *tca9554, tca9554_gpio_t gpio, uint8_t gpio_value);
+tca9554_ret_t tca9554_read_bit(tca9554_t *tca9554, tca9554_gpio_t gpio, uint8_t *gpio_value);
 
 #ifdef __cplusplus
 } // extern "C"
--- design_pattern_factory/components/tca9554_drv/tca9554_drv.c
+++ design_pattern_factory/components/tca9554_drv/tca9554_drv.c
@@ -14,6 +14,7 @@
     Includes
 ******************************************************************************/
 #include <stdint.h>
+#include <stddef.h>
 
 #include "include/tca9554_drv.h"
 /******************************************************************************
@@ -39,7 +40,45 @@
 /******************************************************************************
     Public function definitions
 ******************************************************************************/
-tca9554_ret_t tca9554_init(tca9554_regs_t *reg)
+tca9554_ret_t tca9554_init(tca9554_t *tca9554, uint8_t address)
 {
+	if (NULL == tca9554->i2c_read_byte || NULL == tca9554->i2c_write_byte)
+	{
+		return TCA9554_ERROR;
+	}
+	
+	tca9554->config_register.set.all = 0xFF;
+	tca9554->output_register.set.all = 0xFF;
+	tca9554->polarity_register.set.all = 0x00;
+	tca9554->slave_address = address;
+	
+	tca9554->i2c_write_byte(tca9554->slave_address, TCA9554_CONFIG_REG, tca9554->config_register.set.all);
+	tca9554->i2c_write_byte(tca9554->slave_address, TCA9554_POLARITY_REG, tca9554->polarity_register.set.all);
+	tca9554->i2c_write_byte(tca9554->slave_address, TCA9554_OUTPUT_REG, tca9554->output_register.set.all);
+	
 	return TCA9554_OK;
 }
+
+tca9554_ret_t tca9554_write_reg(tca9554_t *tca9554, register_address_t reg_address, uint8_t reg_value)
+{
+	return (0 == tca9554->i2c_write_byte(tca9554->slave_address, reg_address, reg_value)) ? TCA9554_OK : TCA9554_ERROR;
+}
+
+tca9554_ret_t tca9554_read_reg(tca9554_t *tca9554, register_address_t reg_address, uint8_t *reg_value)
+{
+	
+	return (0 == tca9554->i2c_read_byte(tca9554->slave_address, reg_address, reg_value)) ? TCA9554_OK : TCA9554_ERROR;	
+}
+
+tca9554_ret_t tca9554_write_bit(tca9554_t *tca9554, tca9554_gpio_t gpio, uint8_t gpio_value)
+{
+	return TCA9554_OK;	
+}
+
+tca9554_ret_t tca9554_read_bit(tca9554_t *tca9554, tca9554_gpio_t gpio, uint8_t *gpio_value)
+{
+	return TCA9554_OK;	
+}
+
+
+
